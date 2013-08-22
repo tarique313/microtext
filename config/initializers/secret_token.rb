60b21f97ecdd7c8daf8c20ce09aa5903a13801d6
tarique313 <mahmud.tarique@gmail.com>
2013-08-22 12:04:51 +0600

    Initial comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microtext::Application.config.secret_token = 'abac60bf3ebce83539e522d368e27daa0e9d2b4ad9fa1d45ed7fd2a399ae75582dc10a1b3bdbb2387327bae0e7095862cb299b0c6ecf8cdb81138b4aa0441c5c'
