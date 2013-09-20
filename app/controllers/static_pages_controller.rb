6edeb25e02106f4bfa8601427e7e8bca10bb544c
tarique313 <mahmud.tarique@gmail.com>
2013-09-21 00:00:54 +0600

    Add show comment button

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,8 @@ class StaticPagesController < ApplicationController
   	@micropost = current_user.microposts.build
   	@feed_items = current_user.feed.paginate(page: params[:page])
   end
+
+  
 end
 
   def help
